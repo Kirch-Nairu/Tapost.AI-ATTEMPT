3e07c492028314ce43d95e48c8fa99cef5e14c9c
Kirch Ivan Balite <kirchbalite.careers@gmail.com>
2026-09-06 13:36:49 +0800

    refactor: expose alarm engine adapter

---
+++ src/services/alarm/index.ts
@@ -0,0 +1,4 @@
+import { BrowserAlarmEngine } from './BrowserAlarmEngine';
+
+export const alarmEngine = new BrowserAlarmEngine();
+export type { AlarmCapabilities, AlarmEngine, AlarmPermissionState } from './AlarmEngine';
